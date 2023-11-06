8fedab839e5e147b28ca180bcc2e71a83f2132b1
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-11-06 19:37:45 -0300

    Update prisma client

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,19 +1,20 @@
 import { PrismaClient } from "@prisma/client"
 
 declare global {
-    namespace NodeJS {
-        interface Global {}
+    var cachedPrisma: PrismaClient;
+  }
+  
+  let prisma: PrismaClient;
+  
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+  } else {
+    if (!global.cachedPrisma) {
+      global.cachedPrisma = new PrismaClient();
     }
-}
+    prisma = global.cachedPrisma;
+  }
+  
+  export default prisma;
 
-interface CustomNodeJsGlobal extends NodeJS.Global {
-    prisma: PrismaClient
-}
 
-declare const global: CustomNodeJsGlobal
-
-const prisma = global.prisma || new PrismaClient()
-
-if(process.env.NODE_ENV === "development") global.prisma = prisma
-
-export default prisma
