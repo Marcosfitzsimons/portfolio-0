dcc19321bc7c0705410a6053b7281c2970ccf1cd
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-11-07 14:59:56 -0300

    Change header width

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,9 @@ const fira = Montserrat({ subsets: ["latin"], weight: ["600"] });
 
 const Header = () => {
   return (
-    <div className={`${fira.className} py-4 flex items-center justify-center`}>
+    <div
+      className={`${fira.className} w-full py-4 flex items-center justify-center`}
+    >
       <Link href="/" className="group font-semibold flex items-center gap-0.5">
         <Shell className="w-5 h-5 group-hover:rotate-180 transition-transform" />
         Marcos Fitzsimons
