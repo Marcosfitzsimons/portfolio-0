ead312a27c2b30a95b16b6c4eff03c8948493669
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-11-09 17:37:05 -0300

    Delete header icon

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,3 @@
-import { Shell } from "lucide-react";
 import Link from "next/link";
 import { monserrat } from "./fonts";
 
@@ -8,7 +7,6 @@ const Header = () => {
       className={`${monserrat.className} w-full py-4 flex items-center justify-center`}
     >
       <Link href="/" className="group font-semibold flex items-center gap-0.5">
-        <Shell className="w-5 h-5 group-hover:rotate-180 transition-transform" />
         Marcos Fitzsimons
       </Link>
     </div>
