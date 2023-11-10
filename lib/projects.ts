f730de55c3ecb3b39064d0aa95216cfeaff169b0
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-11-10 11:22:28 -0300

    Add another way to use cache no-store when fetching data

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,12 +1,20 @@
 import prisma from "@/prisma/client";
+// import { unstable_noStore as noStore } from "next/cache";
 
 export const getAllProjects = async () => {
+    // check that...
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    // This is equivalent to in fetch(..., {cache: 'no-store'})
+    // noStore();
+
     const data = await prisma.project.findMany();
 
     return data
 }
 
 export const getSingleProject = async (id: string) => {
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    
     const data = await prisma.project.findUnique({
         where: {
           id: Number(id),
