ea25bd40ba9c50925b176e81f35a63ead4b9302d
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-17 21:39:40 -0300

    Change loading skeleton h eight

--- components/skeletons/single-work-skeleton.tsx
+++ components/skeletons/single-work-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "../ui/skeleton";
 const SingleWorkSkeleton = () => {
   return (
     <div className="flex flex-col items-center gap-5 py-5 sm:w-[80%] sm:mx-auto">
-      <Skeleton className="w-full aspect-video rounded-2xl md:h-[250px]" />
+      <Skeleton className="w-full aspect-video rounded-2xl md:h-[251px]" />
       <Skeleton className="self-start h-7 w-[80%] sm:w-[60%]" />
       <Skeleton className="w-full self-start h-16 " />
       <div className="w-full flex flex-col gap-1">
@@ -12,7 +12,7 @@ const SingleWorkSkeleton = () => {
         <Skeleton className="w-16 h-5 " />
         <Skeleton className="w-full h-5 " />
       </div>
-      <Skeleton className="w-full aspect-video rounded-2xl md:h-[250px]" />
+      <Skeleton className="w-full aspect-video rounded-2xl md:h-[251px]" />
     </div>
   );
 };
