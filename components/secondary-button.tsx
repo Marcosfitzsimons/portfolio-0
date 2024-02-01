5f47172e605c451bcbb3a07caa81b4a41e621045
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-02-01 12:41:03 -0300

    Change secondary button to be reusable

--- components/secondary-button.tsx
+++ components/secondary-button.tsx
@@ -5,7 +5,9 @@ import { ArrowUp, User } from "lucide-react";
 const SecondaryButton = ({
   children,
   icon,
+  href,
 }: {
+  href: string;
   children: React.ReactNode;
   icon: any;
 }) => {
@@ -15,7 +17,7 @@ const SecondaryButton = ({
       variant="secondary"
       className="group relative inline-flex items-center overflow-hidden rounded-2xl pr-[1.1rem] pl-[2.45rem] outline-none transition duration-300 focus:ring-[0.1875rem] focus:ring-purple-400  lg:text-base lg:pl-[3rem] lg:pr-[1rem]"
     >
-      <Link href="/about">
+      <Link href={href}>
         <div className="absolute left-4 translate-y-0 opacity-100 transition duration-300 group-hover:-translate-y-full group-hover:opacity-0">
           {icon}
         </div>
