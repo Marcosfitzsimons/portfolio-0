e5e26204035076edeabb2ce22d1deeda781aa02d
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-02-05 18:52:10 -0300

    Add validations folder

---
+++ lib/validations/question.ts
@@ -0,0 +1,9 @@
+import * as z from "zod"
+
+export const questionSchema = z.object({
+  question: z.string().min(3, {
+    message: "Question must be at least 3 characters",
+  }).max(160, {
+    message: "Question cannot be longer than 160 characterest"
+  })
+})
